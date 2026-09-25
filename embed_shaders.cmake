ebf91d2b972ef9164bfa73782f0e1c629a2b05f6
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-25 18:47:37 +0800

    Add files via upload

---
+++ embed_shaders.cmake
@@ -0,0 +1,23 @@
+file(READ "${INPUT_VERT}" VERT_HEX HEX)
+file(READ "${INPUT_FRAG}" FRAG_HEX HEX)
+
+string(LENGTH "${VERT_HEX}" VERT_LEN)
+string(LENGTH "${FRAG_HEX}" FRAG_LEN)
+
+file(WRITE "${OUTPUT}" "#pragma once\n\n")
+
+file(APPEND "${OUTPUT}" "static const unsigned char vertShaderBytes[] = {\n")
+math(EXPR VERT_LAST "${VERT_LEN} - 2")
+foreach(i RANGE 0 ${VERT_LAST} 2)
+    string(SUBSTRING "${VERT_HEX}" ${i} 2 BYTE)
+    file(APPEND "${OUTPUT}" "0x${BYTE}, ")
+endforeach()
+file(APPEND "${OUTPUT}" "0};\n\n")
+
+file(APPEND "${OUTPUT}" "static const unsigned char fragShaderBytes[] = {\n")
+math(EXPR FRAG_LAST "${FRAG_LEN} - 2")
+foreach(i RANGE 0 ${FRAG_LAST} 2)
+    string(SUBSTRING "${FRAG_HEX}" ${i} 2 BYTE)
+    file(APPEND "${OUTPUT}" "0x${BYTE}, ")
+endforeach()
+file(APPEND "${OUTPUT}" "0};\n")
