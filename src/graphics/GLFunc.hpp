be2cb4b52d551dfb77b0d2cec05cf602974b2e0c
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 12:52:33 +0800

    Update GLFunc.hpp

--- src/graphics/GLFunc.hpp
+++ src/graphics/GLFunc.hpp
@@ -7,24 +7,24 @@
 #if TARGET_OS_IPHONE || TARGET_OS_SIMULATOR
 
 // ============================================================
-// iOS
-// ============================================================
-//
-// FixedFunctionGL uses the OpenGL ES 1.1 fixed-function pipeline.
-// Keep the ES2 shader function pointer typedefs below because
-// other parts of the project may still use them.
-//
+// iOS OpenGL ES 1.1
+// Used by FixedFunctionGL
 // ============================================================
 
 #include <OpenGLES/ES1/gl.h>
 #include <OpenGLES/ES1/glext.h>
 
-// ------------------------------------------------------------
-// Compatibility types
-// ------------------------------------------------------------
+// ============================================================
+// Basic compatibility types
+// ============================================================
 
+#ifndef GLclampf
 typedef GLfloat GLclampf;
+#endif
+
+#ifndef GLclampd
 typedef GLfloat GLclampd;
+#endif
 
 #ifndef GLAPIENTRY
 #define GLAPIENTRY
@@ -34,50 +34,95 @@ typedef GLfloat GLclampd;
 typedef unsigned int GLbitfield;
 #endif
 
-// ------------------------------------------------------------
-// OpenGL ES 2.x function pointer types
+// ============================================================
+// OpenGL ES 2.0 shader constants
 //
-// ES1 headers do not provide these types, but the project still
-// has shader-related entries in GLFuncTable.
-// ------------------------------------------------------------
+// WebGL.cpp uses these constants, while FixedFunctionGL
+// uses the OpenGL ES 1.1 fixed-function pipeline.
+//
+// We only need the constants here. The actual ES2 functions
+// are resolved dynamically through GLFuncTable.
+// ============================================================
+
+#ifndef GL_COMPILE_STATUS
+#define GL_COMPILE_STATUS 0x8B81
+#endif
+
+#ifndef GL_INFO_LOG_LENGTH
+#define GL_INFO_LOG_LENGTH 0x8B84
+#endif
+
+#ifndef GL_LINK_STATUS
+#define GL_LINK_STATUS 0x8B82
+#endif
+
+#ifndef GL_VERTEX_SHADER
+#define GL_VERTEX_SHADER 0x8B31
+#endif
+
+#ifndef GL_FRAGMENT_SHADER
+#define GL_FRAGMENT_SHADER 0x8B30
+#endif
+
+// ============================================================
+// OpenGL ES 2.0 shader function pointer types
+// ============================================================
 
 #ifndef PFNGLATTACHSHADERPROC
-typedef void (*PFNGLATTACHSHADERPROC)(GLuint program, GLuint shader);
+typedef void (*PFNGLATTACHSHADERPROC)(
+    GLuint program,
+    GLuint shader
+);
 #endif
 
 #ifndef PFNGLBINDATTRIBLOCATIONPROC
 typedef void (*PFNGLBINDATTRIBLOCATIONPROC)(
     GLuint program,
     GLuint index,
-    const GLchar *name);
+    const GLchar *name
+);
 #endif
 
 #ifndef PFNGLCOMPILESHADERPROC
-typedef void (*PFNGLCOMPILESHADERPROC)(GLuint shader);
+typedef void (*PFNGLCOMPILESHADERPROC)(
+    GLuint shader
+);
 #endif
 
 #ifndef PFNGLCREATEPROGRAMPROC
-typedef GLuint (*PFNGLCREATEPROGRAMPROC)(void);
+typedef GLuint (*PFNGLCREATEPROGRAMPROC)(
+    void
+);
 #endif
 
 #ifndef PFNGLCREATESHADERPROC
-typedef GLuint (*PFNGLCREATESHADERPROC)(GLenum type);
+typedef GLuint (*PFNGLCREATESHADERPROC)(
+    GLenum type
+);
 #endif
 
 #ifndef PFNGLDELETEPROGRAMPROC
-typedef void (*PFNGLDELETEPROGRAMPROC)(GLuint program);
+typedef void (*PFNGLDELETEPROGRAMPROC)(
+    GLuint program
+);
 #endif
 
 #ifndef PFNGLDELETESHADERPROC
-typedef void (*PFNGLDELETESHADERPROC)(GLuint shader);
+typedef void (*PFNGLDELETESHADERPROC)(
+    GLuint shader
+);
 #endif
 
 #ifndef PFNGLDISABLEVERTEXATTRIBARRAYPROC
-typedef void (*PFNGLDISABLEVERTEXATTRIBARRAYPROC)(GLuint index);
+typedef void (*PFNGLDISABLEVERTEXATTRIBARRAYPROC)(
+    GLuint index
+);
 #endif
 
 #ifndef PFNGLENABLEVERTEXATTRIBARRAYPROC
-typedef void (*PFNGLENABLEVERTEXATTRIBARRAYPROC)(GLuint index);
+typedef void (*PFNGLENABLEVERTEXATTRIBARRAYPROC)(
+    GLuint index
+);
 #endif
 
 #ifndef PFNGLGETPROGRAMINFOLOGPROC
@@ -85,14 +130,16 @@ typedef void (*PFNGLGETPROGRAMINFOLOGPROC)(
     GLuint program,
     GLsizei bufSize,
     GLsizei *length,
-    GLchar *infoLog);
+    GLchar *infoLog
+);
 #endif
 
 #ifndef PFNGLGETPROGRAMIVPROC
 typedef void (*PFNGLGETPROGRAMIVPROC)(
     GLuint program,
     GLenum pname,
-    GLint *params);
+    GLint *params
+);
 #endif
 
 #ifndef PFNGLGETSHADERINFOLOGPROC
@@ -100,24 +147,29 @@ typedef void (*PFNGLGETSHADERINFOLOGPROC)(
     GLuint shader,
     GLsizei bufSize,
     GLsizei *length,
-    GLchar *infoLog);
+    GLchar *infoLog
+);
 #endif
 
 #ifndef PFNGLGETSHADERIVPROC
 typedef void (*PFNGLGETSHADERIVPROC)(
     GLuint shader,
     GLenum pname,
-    GLint *params);
+    GLint *params
+);
 #endif
 
 #ifndef PFNGLGETUNIFORMLOCATIONPROC
 typedef GLint (*PFNGLGETUNIFORMLOCATIONPROC)(
     GLuint program,
-    const GLchar *name);
+    const GLchar *name
+);
 #endif
 
 #ifndef PFNGLLINKPROGRAMPROC
-typedef void (*PFNGLLINKPROGRAMPROC)(GLuint program);
+typedef void (*PFNGLLINKPROGRAMPROC)(
+    GLuint program
+);
 #endif
 
 #ifndef PFNGLSHADERSOURCEPROC
@@ -125,19 +177,22 @@ typedef void (*PFNGLSHADERSOURCEPROC)(
     GLuint shader,
     GLsizei count,
     const GLchar *const *string,
-    const GLint *length);
+    const GLint *length
+);
 #endif
 
 #ifndef PFNGLUNIFORM1FPROC
 typedef void (*PFNGLUNIFORM1FPROC)(
     GLint location,
-    GLfloat v0);
+    GLfloat v0
+);
 #endif
 
 #ifndef PFNGLUNIFORM1IPROC
 typedef void (*PFNGLUNIFORM1IPROC)(
     GLint location,
-    GLint v0);
+    GLint v0
+);
 #endif
 
 #ifndef PFNGLUNIFORM4FPROC
@@ -146,7 +201,8 @@ typedef void (*PFNGLUNIFORM4FPROC)(
     GLfloat v0,
     GLfloat v1,
     GLfloat v2,
-    GLfloat v3);
+    GLfloat v3
+);
 #endif
 
 #ifndef PFNGLUNIFORMMATRIX4FVPROC
@@ -154,11 +210,14 @@ typedef void (*PFNGLUNIFORMMATRIX4FVPROC)(
     GLint location,
     GLsizei count,
     GLboolean transpose,
-    const GLfloat *value);
+    const GLfloat *value
+);
 #endif
 
 #ifndef PFNGLUSEPROGRAMPROC
-typedef void (*PFNGLUSEPROGRAMPROC)(GLuint program);
+typedef void (*PFNGLUSEPROGRAMPROC)(
+    GLuint program
+);
 #endif
 
 #ifndef PFNGLVERTEXATTRIBPOINTERPROC
@@ -168,143 +227,163 @@ typedef void (*PFNGLVERTEXATTRIBPOINTERPROC)(
     GLenum type,
     GLboolean normalized,
     GLsizei stride,
-    const GLvoid *pointer);
+    const GLvoid *pointer
+);
 #endif
 
 #else
 
-// macOS
+// ============================================================
+// Desktop OpenGL
+// ============================================================
+
 #include <SDL2/SDL_opengl.h>
 
 #endif
 
 #else
 
+// ============================================================
 // Non-Apple platforms
+// ============================================================
+
 #include <SDL2/SDL_opengl.h>
 
 #endif
 
 
-// ============================================================
-// GLFuncTable
-// ============================================================
-//
-// Function pointers for OpenGL functions used in EoSD.
-//
-// Windows opengl32 only exposes OpenGL 1.1 directly, while the
-// project uses some newer OpenGL functionality. SDL_GL_GetProcAddress
-// is therefore used to resolve functions at runtime.
-//
-// On iOS, ResolveFunctions(true) resolves the OpenGL ES path.
-//
-// ============================================================
+// ================================================================
+// GL Function Table
+// ================================================================
 
 struct GLFuncTable
 {
     void ResolveFunctions(bool glesContext);
 
-    // --------------------------------------------------------
-    // Functions whose GL/GLES signatures differ
-    // --------------------------------------------------------
+    // ============================================================
+    // Depth functions
+    // ============================================================
 
     void glClearDepthf(GLclampf depth);
+    void glDepthRangef(GLclampf near_val, GLclampf far_val);
 
-    void glDepthRangef(
-        GLclampf near_val,
-        GLclampf far_val);
+    // ============================================================
+    // OpenGL / OpenGL ES fixed-function API
+    // ============================================================
 
-    // --------------------------------------------------------
-    // OpenGL / OpenGL ES fixed-function functions
-    // --------------------------------------------------------
-
-    void(GLAPIENTRY *glAlphaFunc)(
-        GLenum func,
-        GLclampf ref);
+    void(GLAPIENTRY *glAlphaFunc)(GLenum func, GLclampf ref);
 
     void(GLAPIENTRY *glBindTexture)(
         GLenum target,
-        GLuint texture);
+        GLuint texture
+    );
 
     void(GLAPIENTRY *glBlendFunc)(
         GLenum sfactor,
-        GLenum dfactor);
+        GLenum dfactor
+    );
 
     void(GLAPIENTRY *glClear)(
-        GLbitfield mask);
+        GLbitfield mask
+    );
 
     void(GLAPIENTRY *glClearColor)(
         GLclampf red,
         GLclampf green,
         GLclampf blue,
-        GLclampf alpha);
+        GLclampf alpha
+    );
 
     void(GLAPIENTRY *glColorPointer)(
         GLint size,
         GLenum type,
         GLsizei stride,
-        const GLvoid *ptr);
+        const GLvoid *ptr
+    );
 
     void(GLAPIENTRY *glDeleteTextures)(
         GLsizei n,
-        const GLuint *textures);
+        const GLuint *textures
+    );
 
     void(GLAPIENTRY *glDepthFunc)(
-        GLenum func);
+        GLenum func
+    );
 
     void(GLAPIENTRY *glDepthMask)(
-        GLboolean flag);
+        GLboolean flag
+    );
 
     void(GLAPIENTRY *glDisableClientState)(
-        GLenum cap);
+        GLenum cap
+    );
 
     void(GLAPIENTRY *glDrawArrays)(
         GLenum mode,
         GLint first,
-        GLsizei count);
+        GLsizei count
+    );
 
     void(GLAPIENTRY *glEnable)(
-        GLenum cap);
+        GLenum cap
+    );
 
     void(GLAPIENTRY *glEnableClientState)(
-        GLenum cap);
+        GLenum cap
+    );
 
     void(GLAPIENTRY *glFogf)(
         GLenum pname,
-        GLfloat param);
+        GLfloat param
+    );
 
     void(GLAPIENTRY *glFogfv)(
         GLenum pname,
-        const GLfloat *params);
+        const GLfloat *params
+    );
 
     void(GLAPIENTRY *glGenTextures)(
         GLsizei n,
-        GLuint *textures);
+        GLuint *textures
+    );
 
-    GLenum(GLAPIENTRY *glGetError)(void);
+    GLenum(GLAPIENTRY *glGetError)(
+        void
+    );
 
     void(GLAPIENTRY *glGetFloatv)(
         GLenum pname,
-        GLfloat *params);
+        GLfloat *params
+    );
 
     void(GLAPIENTRY *glGetIntegerv)(
         GLenum pname,
-        GLint *params);
+        GLint *params
+    );
 
-    void(GLAPIENTRY *glLoadIdentity)(void);
+    void(GLAPIENTRY *glLoadIdentity)(
+        void
+    );
 
     void(GLAPIENTRY *glLoadMatrixf)(
-        const GLfloat *m);
+        const GLfloat *m
+    );
 
     void(GLAPIENTRY *glMatrixMode)(
-        GLenum mode);
+        GLenum mode
+    );
 
     void(GLAPIENTRY *glMultMatrixf)(
-        const GLfloat *m);
+        const GLfloat *m
+    );
 
-    void(GLAPIENTRY *glPopMatrix)(void);
+    void(GLAPIENTRY *glPopMatrix)(
+        void
+    );
 
-    void(GLAPIENTRY *glPushMatrix)(void);
+    void(GLAPIENTRY *glPushMatrix)(
+        void
+    );
 
     void(GLAPIENTRY *glReadPixels)(
         GLint x,
@@ -313,26 +392,31 @@ struct GLFuncTable
         GLsizei height,
         GLenum format,
         GLenum type,
-        GLvoid *pixels);
+        GLvoid *pixels
+    );
 
     void(GLAPIENTRY *glShadeModel)(
-        GLenum mode);
+        GLenum mode
+    );
 
     void(GLAPIENTRY *glTexCoordPointer)(
         GLint size,
         GLenum type,
         GLsizei stride,
-        const GLvoid *ptr);
+        const GLvoid *ptr
+    );
 
     void(GLAPIENTRY *glTexEnvfv)(
         GLenum target,
         GLenum pname,
-        const GLfloat *params);
+        const GLfloat *params
+    );
 
     void(GLAPIENTRY *glTexEnvi)(
         GLenum target,
         GLenum pname,
-        GLint param);
+        GLint param
+    );
 
     void(GLAPIENTRY *glTexImage2D)(
         GLenum target,
@@ -343,12 +427,14 @@ struct GLFuncTable
         GLint border,
         GLenum format,
         GLenum type,
-        const GLvoid *pixels);
+        const GLvoid *pixels
+    );
 
     void(GLAPIENTRY *glTexParameteri)(
         GLenum target,
         GLenum pname,
-        GLint param);
+        GLint param
+    );
 
     void(GLAPIENTRY *glTexSubImage2D)(
         GLenum target,
@@ -359,32 +445,27 @@ struct GLFuncTable
         GLsizei height,
         GLenum format,
         GLenum type,
-        const GLvoid *pixels);
+        const GLvoid *pixels
+    );
 
     void(GLAPIENTRY *glVertexPointer)(
         GLint size,
         GLenum type,
         GLsizei stride,
-        const GLvoid *ptr);
+        const GLvoid *ptr
+    );
 
     void(GLAPIENTRY *glViewport)(
         GLint x,
         GLint y,
         GLsizei width,
-        GLsizei height);
+        GLsizei height
+    );
 
 
-    // ========================================================
-    // GL / GLES 2.x shader functions
-    // ========================================================
-    //
-    // These are kept because other parts of the project may
-    // still use the shader-based renderer.
-    //
-    // They are not used by the OpenGL ES 1.1 fixed-function
-    // renderer itself.
-    //
-    // ========================================================
+    // ============================================================
+    // OpenGL ES 2.0 / WebGL shader functions
+    // ============================================================
 
     PFNGLATTACHSHADERPROC glAttachShader;
 
@@ -448,35 +529,44 @@ struct GLFuncTable
 
 private:
 
-    // --------------------------------------------------------
-    // GLES versions for functions whose signatures differ
-    // --------------------------------------------------------
+    // ============================================================
+    // GLES depth functions
+    // ============================================================
 
     void(GLAPIENTRY *glClearDepthf_ptr)(
-        GLclampf depth);
+        GLclampf depth
+    );
 
     void(GLAPIENTRY *glDepthRangef_ptr)(
         GLclampf near_val,
-        GLclampf far_val);
+        GLclampf far_val
+    );
 
 
-    // --------------------------------------------------------
-    // Desktop OpenGL versions
-    // --------------------------------------------------------
+    // ============================================================
+    // Desktop OpenGL depth functions
+    // ============================================================
 
     void(GLAPIENTRY *glClearDepth)(
-        GLclampd depth);
+        GLclampd depth
+    );
 
     void(GLAPIENTRY *glDepthRange)(
         GLclampd near_val,
-        GLclampd far_val);
+        GLclampd far_val
+    );
 
 
-    // --------------------------------------------------------
-    // Current rendering API
-    // --------------------------------------------------------
+    // ============================================================
+    // Current context type
+    // ============================================================
 
     bool isGlesContext;
 };
+
+
+// ================================================================
+// Global function table
+// ================================================================
 
 extern GLFuncTable g_glFuncTable;
