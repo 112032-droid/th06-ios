6586a33d244abf15a934d51a8bf5ff40b226c7fd
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 17:19:36 +0800

    Update main_ios.mm

--- src/main_ios.mm
+++ src/main_ios.mm
@@ -1,13 +1,43 @@
 // ============================================================
 // TH06 iOS Entry Point
 // ============================================================
-
 #import <UIKit/UIKit.h>
-
+#import <Foundation/Foundation.h>
+#include <unistd.h>
+#include <limits.h>
 int main(int argc, char *argv[])
 {
     @autoreleasepool
     {
+        // ----------------------------------------------------
+        // Get application bundle resource path
+        // ----------------------------------------------------
+        NSString *resourcePath =
+            [[NSBundle mainBundle] resourcePath];
+        if (resourcePath != nil)
+        {
+            const char *path =
+                [resourcePath fileSystemRepresentation];
+            if (path != nullptr)
+            {
+                // ------------------------------------------------
+                // Make the App Bundle the current working directory.
+                //
+                // This allows the game to find files such as:
+                //
+                // IN.DAT
+                // ST.DAT
+                // TL.DAT
+                // _MD.DAT
+                // _ED.DAT
+                // _CM.DAT
+                // ------------------------------------------------
+                chdir(path);
+            }
+        }
+        // ----------------------------------------------------
+        // Start iOS application
+        // ----------------------------------------------------
         return UIApplicationMain(
             argc,
             argv,
