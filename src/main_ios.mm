e97ea92e7f84a0e02d064e79b2b606f95b8b7cbe
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 17:42:52 +0800

    Update main_ios.mm

--- src/main_ios.mm
+++ src/main_ios.mm
@@ -1,16 +1,45 @@
 // ============================================================
 // TH06 iOS Entry Point
 // ============================================================
+//
+// iOS entry point for Touhou TH06.
+//
+// The actual game entry point is:
+//
+//     th06_main(argc, argv)
+//
+// This file:
+//   1. Gets the application Bundle resource path
+//   2. Changes the current working directory to the Bundle
+//   3. Starts SDL2's UIKit application system
+//   4. Passes control to th06_main()
+//
+// ============================================================
+#define SDL_MAIN_HANDLED
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
 #include <unistd.h>
 #include <limits.h>
+// ============================================================
+// TH06 game entry point
+//
+// Defined in:
+//     src/main.cpp
+//
+// main() in main.cpp was renamed to th06_main()
+// ============================================================
+extern int th06_main(int argc, char *argv[]);
+// ============================================================
+// iOS Entry Point
+// ============================================================
 int main(int argc, char *argv[])
 {
     @autoreleasepool
     {
         // ----------------------------------------------------
-        // Get application bundle resource path
+        // Get application Bundle resource path
         // ----------------------------------------------------
         NSString *resourcePath =
             [[NSBundle mainBundle] resourcePath];
@@ -23,7 +52,7 @@ int main(int argc, char *argv[])
                 // ------------------------------------------------
                 // Make the App Bundle the current working directory.
                 //
-                // This allows the game to find files such as:
+                // This allows TH06 to find:
                 //
                 // IN.DAT
                 // ST.DAT
@@ -31,18 +60,39 @@ int main(int argc, char *argv[])
                 // _MD.DAT
                 // _ED.DAT
                 // _CM.DAT
+                //
+                // and directories such as:
+                //
+                // bgm/
+                // data/
+                // etc.
                 // ------------------------------------------------
-                chdir(path);
+                if (chdir(path) != 0)
+                {
+                    NSLog(
+                        @"TH06: Failed to change working directory: %s",
+                        path
+                    );
+                }
+                else
+                {
+                    NSLog(
+                        @"TH06: Working directory: %s",
+                        path
+                    );
+                }
             }
         }
         // ----------------------------------------------------
-        // Start iOS application
+        // Start SDL2 UIKit application
+        //
+        // SDL_UIKitRunApp() creates the iOS application
+        // environment and calls th06_main().
         // ----------------------------------------------------
-        return UIApplicationMain(
+        return SDL_UIKitRunApp(
             argc,
             argv,
-            nil,
-            nil
+            th06_main
         );
     }
 }
