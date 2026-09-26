a04d15feb11dce5d80d6fb3ceb7456d0f8539cdc
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 17:40:37 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mouse.h>
 #include <cstdio>
-
 #include "AnmManager.hpp"
 #include "Chain.hpp"
 #include "FileSystem.hpp"
@@ -13,74 +12,74 @@
 #include "ZunResult.hpp"
 #include "i18n.hpp"
 #include "utils.hpp"
-
-int main(int argc, char *argv[])
+// ============================================================
+// TH06 iOS / Cross-platform game entry point
+//
+// NOTE:
+// The original main() has been renamed to th06_main().
+//
+// main_ios.mm will call th06_main() from the iOS entry point.
+// ============================================================
+int th06_main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-
     i32 renderResult = 0;
-    //    MSG msg;
-    //    i32 waste1, waste2, waste3, waste4, waste5, waste6;
-
-    //    if (utils::CheckForRunningGameInstance())
-    //    {
-    //        g_GameErrorContext.Flush();
-    //
-    //        return 1;
-    //    }
-
-    //    g_Supervisor.hInstance = hInstance;
-
+    // --------------------------------------------------------
+    // Load configuration
+    // --------------------------------------------------------
     if (g_Supervisor.LoadConfig(TH_CONFIG_FILE) != ZUN_SUCCESS)
     {
         g_GameErrorContext.Flush();
         return -1;
     }
-
-    //    if (GameWindow::InitD3dInterface())
-    //    {
-    //        g_GameErrorContext.Flush();
-    //        return 1;
-    //    }
-
-    //    SystemParametersInfo(SPI_GETSCREENSAVEACTIVE, 0, &g_GameWindow.screenSaveActive, 0);
-    //    SystemParametersInfo(SPI_GETLOWPOWERACTIVE, 0, &g_GameWindow.lowPowerActive, 0);
-    //    SystemParametersInfo(SPI_GETPOWEROFFACTIVE, 0, &g_GameWindow.powerOffActive, 0);
-    //    SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, 0, NULL, SPIF_SENDCHANGE);
-    //    SystemParametersInfo(SPI_SETLOWPOWERACTIVE, 0, NULL, SPIF_SENDCHANGE);
-    //    SystemParametersInfo(SPI_SETPOWEROFFACTIVE, 0, NULL, SPIF_SENDCHANGE);
-
 restart:
+    // --------------------------------------------------------
+    // Create SDL game window
+    // --------------------------------------------------------
     GameWindow::CreateGameWindow();
-
+    // --------------------------------------------------------
+    // Create animation manager
+    // --------------------------------------------------------
     g_AnmManager = new AnmManager();
-
+    // --------------------------------------------------------
+    // Initialize rendering
+    // --------------------------------------------------------
     if (GameWindow::InitD3dRendering() != ZUN_SUCCESS)
     {
         g_GameErrorContext.Flush();
         return 1;
     }
-
+    // --------------------------------------------------------
+    // Initialize sound
+    // --------------------------------------------------------
     g_SoundPlayer.InitializeDSound();
+    // --------------------------------------------------------
+    // Initialize controller / keyboard
+    // --------------------------------------------------------
     Controller::GetJoystickCaps();
     Controller::ResetKeyboard();
-
+    // --------------------------------------------------------
+    // Register game state chain
+    // --------------------------------------------------------
     if (Supervisor::RegisterChain() != ZUN_SUCCESS)
     {
         goto stop;
     }
+    // --------------------------------------------------------
+    // Hide mouse cursor when not running windowed
+    // --------------------------------------------------------
     if (!g_Supervisor.cfg.windowed)
     {
         SDL_ShowCursor(SDL_DISABLE);
     }
-
     g_GameWindow.curFrame = 0;
-
+    // ========================================================
+    // Main game loop
+    // ========================================================
     while (true)
     {
         SDL_Event e;
-
         while (SDL_PollEvent(&e))
         {
             if (e.type == SDL_QUIT)
@@ -88,75 +87,49 @@ restart:
                 goto stop;
             }
         }
-
         renderResult = g_GameWindow.Render();
         if (renderResult != 0)
         {
             break;
         }
-
-        //        SDL_Delay(1000.0f / 60.0f);
-
-        //        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-        //        {
-        //            TranslateMessage(&msg);
-        //            DispatchMessage(&msg);
-        //        }
-        //        else
-        //        {
-        //            testCoopLevelRes = g_Supervisor.d3dDevice->TestCooperativeLevel();
-        //            if (testCoopLevelRes == D3D_OK)
-        //            {
-        //                renderResult = g_GameWindow.Render();
-        //                if (renderResult != 0)
-        //                {
-        //                    goto stop;
-        //                }
-        //            }
-        //            else if (testCoopLevelRes == D3DERR_DEVICENOTRESET)
-        //            {
-        //                g_AnmManager->ReleaseSurfaces();
-        //                testResetRes = g_Supervisor.d3dDevice->Reset(&g_Supervisor.presentParameters);
-        //                if (testResetRes != 0)
-        //                {
-        //                    goto stop;
-        //                }
-        //                GameWindow::InitD3dDevice();
-        //                g_Supervisor.unk198 = 3;
-        //            }
-        //        }
     }
-
+    // ========================================================
+    // Shutdown
+    // ========================================================
 stop:
     g_Chain.Release();
     g_SoundPlayer.Release();
-
     delete g_AnmManager;
     g_AnmManager = NULL;
-
     if (g_GfxBackend != NULL)
+    {
         delete g_GfxBackend;
+        g_GfxBackend = NULL;
+    }
     SDL_Quit();
-
+    // --------------------------------------------------------
+    // Restart requested by game options
+    // --------------------------------------------------------
     if (renderResult == 2)
     {
         g_GameErrorContext.ResetContext();
-
-        g_GameErrorContext.Log(TH_ERR_OPTION_CHANGED_RESTART);
-
+        g_GameErrorContext.Log(
+            TH_ERR_OPTION_CHANGED_RESTART
+        );
         if (!g_Supervisor.cfg.windowed)
         {
             SDL_ShowCursor(SDL_ENABLE);
         }
-
         goto restart;
     }
-
-    FileSystem::WriteDataToFile(TH_CONFIG_FILE, &g_Supervisor.cfg, sizeof(g_Supervisor.cfg));
-    //    SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, g_GameWindow.screenSaveActive, NULL, SPIF_SENDCHANGE);
-    //    SystemParametersInfo(SPI_SETLOWPOWERACTIVE, g_GameWindow.lowPowerActive, NULL, SPIF_SENDCHANGE);
-    //    SystemParametersInfo(SPI_SETPOWEROFFACTIVE, g_GameWindow.powerOffActive, NULL, SPIF_SENDCHANGE);
-
+    // --------------------------------------------------------
+    // Save configuration
+    // --------------------------------------------------------
+    FileSystem::WriteDataToFile(
+        TH_CONFIG_FILE,
+        &g_Supervisor.cfg,
+        sizeof(g_Supervisor.cfg)
+    );
     SDL_ShowCursor(SDL_ENABLE);
     g_GameErrorContext.Flush();
     return 0;
