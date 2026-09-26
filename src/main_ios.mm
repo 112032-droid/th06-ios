adcccb9597c4d3696d968e9d0b2c86f3db39b397
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 13:10:25 +0800

    Create main_ios.mm

---
+++ src/main_ios.mm
@@ -0,0 +1,18 @@
+// ============================================================
+// TH06 iOS Entry Point
+// ============================================================
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(
+            argc,
+            argv,
+            nil,
+            nil
+        );
+    }
+}
