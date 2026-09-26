f4dd46dc9c2b234c738c9604bd1cba2957e0af21
112032-droid <112032@mail.hwjh.ylc.edu.tw>
2026-09-26 09:05:53 +0800

    Update embed_shaders.cmake

--- cmake/embed_shaders.cmake
+++ cmake/embed_shaders.cmake
@@ -6,18 +6,30 @@ string(LENGTH "${FRAG_HEX}" FRAG_LEN)
 
 file(WRITE "${OUTPUT}" "#pragma once\n\n")
 
+# Vertex shader
 file(APPEND "${OUTPUT}" "static const unsigned char vertShaderBytes[] = {\n")
-math(EXPR VERT_LAST "${VERT_LEN} - 2")
-foreach(i RANGE 0 ${VERT_LAST} 2)
-    string(SUBSTRING "${VERT_HEX}" ${i} 2 BYTE)
-    file(APPEND "${OUTPUT}" "0x${BYTE}, ")
-endforeach()
+
+if(VERT_LEN GREATER 0)
+    math(EXPR VERT_LAST "${VERT_LEN} - 2")
+
+    foreach(i RANGE 0 ${VERT_LAST} 2)
+        string(SUBSTRING "${VERT_HEX}" ${i} 2 BYTE)
+        file(APPEND "${OUTPUT}" "0x${BYTE}, ")
+    endforeach()
+endif()
+
 file(APPEND "${OUTPUT}" "0};\n\n")
 
+# Fragment shader
 file(APPEND "${OUTPUT}" "static const unsigned char fragShaderBytes[] = {\n")
-math(EXPR FRAG_LAST "${FRAG_LEN} - 2")
-foreach(i RANGE 0 ${FRAG_LAST} 2)
-    string(SUBSTRING "${FRAG_HEX}" ${i} 2 BYTE)
-    file(APPEND "${OUTPUT}" "0x${BYTE}, ")
-endforeach()
+
+if(FRAG_LEN GREATER 0)
+    math(EXPR FRAG_LAST "${FRAG_LEN} - 2")
+
+    foreach(i RANGE 0 ${FRAG_LAST} 2)
+        string(SUBSTRING "${FRAG_HEX}" ${i} 2 BYTE)
+        file(APPEND "${OUTPUT}" "0x${BYTE}, ")
+    endforeach()
+endif()
+
 file(APPEND "${OUTPUT}" "0};\n")
